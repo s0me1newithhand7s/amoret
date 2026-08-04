c7b009d493de1de2f623431c2a7b5b53fff94c74
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-05 02:54:34 +0300

    chore(repo): bump version
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- flake.nix
+++ flake.nix
@@ -140,7 +140,7 @@
         packages = {
           "default" = pkgs.rustPlatform.buildRustPackage {
             pname = "amoret";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
             cargoLock = {
               lockFile = ./Cargo.lock;
