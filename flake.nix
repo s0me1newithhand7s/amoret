60aa234cccf31b21f86398734ef567ef4063a767
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-05 04:06:51 +0300

    feat(repo): add amoret bincache
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- flake.nix
+++ flake.nix
@@ -27,12 +27,14 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
+      "https://amoret.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "amoret.cachix.org-1:VqDd0+f3goUOwg4J1xCOz7Xg6yhjDqM/4cnyyuDa7co="
     ];
   };
 
@@ -278,6 +280,7 @@
                 pull = [
                   "nix-community"
                   "devenv"
+                  "amoret"
                 ];
               };
 
